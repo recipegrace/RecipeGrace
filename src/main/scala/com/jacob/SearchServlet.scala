15d3f5c62b3735ae50b8f58ee981588087f03cd1
Ferosh.Jacob <Ferosh.Jacob@careerbuilder.com>
2014-03-29 22:55:42 -0400

    Initial commit again

---
+++ src/main/scala/com/jacob/SearchServlet.scala
@@ -0,0 +1,17 @@
+package com.jacob
+
+import org.scalatra._
+import scalate.ScalateSupport
+
+class SearchServlet extends RecipegraceStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+  
+}
