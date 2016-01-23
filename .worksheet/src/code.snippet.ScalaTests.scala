c9e6592ac2c1c42abde967b3e884834619f09e2d
fxj6302 <ferosh_jacob@homedepot.com>
2016-01-23 07:58:37 -0500

    remove unnecassary directories

--- .worksheet/src/code.snippet.ScalaTests.scala
+++
@@ -1,8 +0,0 @@
-package code.snippet
-
-object ScalaTests {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(85); 
-  println("Welcome to the Scala worksheet");$skip(22); 
-  val str = "(hello)";System.out.println("""str  : String = """ + $show(str ))}
-
- 
-}
