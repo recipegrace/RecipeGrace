4d52d882c24acf3fee6ec8e9eb26328ddd237c28
feroshjacob <feroshjacob@gmail.com>
2016-01-31 21:43:30 -0500

    removed in memory directory tests

--- core/src/test/scala/com/recipegrace/web/IndexHelperTest.scala
+++ core/src/test/scala/com/recipegrace/web/IndexHelperTest.scala
@@ -13,30 +13,15 @@ class IndexHelperTest extends BaseTest with TestHelper {
 
  
     
-  def indexAndSearch(directory :Directory) ={
+  test("index based test "){
    
-    createIndex(List(testRecipe), directory)
+   // createIndex(List(testRecipe), directory)
 
+    println(indexPath)
+    val directory = FSDirectory.open(new File(indexPath))
      searchRecipeSize("apple",  directory) should  equal(1)
      searchRecipeSize("mango",  directory) should equal(0)
 
     searchRecipes("butter", 0, 10, directory)(0).getOriginalLines.trim should  equal(recipeLines.trim)
-  }
-  test("RAM Test: Index and Search") {
-
-   indexAndSearch(new RAMDirectory)
-  
-  }
-  test("File Test: Index and Search") {
-
-     
-  
-    for {
-      files <- Option(new File(".tests").listFiles)
-      file <- files
-      if file.exists()
-    } file.delete()
-     indexAndSearch( FSDirectory.open(new File(".tests")))
-
   }
 }
